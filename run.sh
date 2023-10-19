da6ef0097f0ce31c43a525b3655482dcaab3c92d
Casper Schipper <casper.schipper@gmail.com>
2023-10-19 14:52:26 +0200

    ok let's continue

--- run.sh
+++ run.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
-
+set -e
 rm internal_research.json
+rm enriched.json
 wget keywords.sarconference2016.net/internal_research.json
 elm-cli run src/Main.elm
+cp enriched.json ../rckeywords/enriched.json
+
+cd screenshots
+ocaml structure_extract.ml > screenshots.json
