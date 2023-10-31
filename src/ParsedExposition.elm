7d7de2cf0da5e5b1ef3739d0214683c2477a776c
Casper Schipper <casper.schipper@gmail.com>
2023-10-31 13:15:52 +0100

    added Json Parser stub for parsing parsed expositions

---
+++ src/ParsedExposition.elm
@@ -0,0 +1,104 @@
+module ParsedExposition exposing (..)
+
+import Dict exposing (Dict)
+import Json.Decode as D
+import Json.Encode as E
+import Research exposing (ExpositionID)
+
+
+type alias Dimensions =
+    { x : Int, y : Int, w : Int, h : Int }
+
+
+type alias PageData =
+    { id : PageID
+    , tools : List Tool
+    }
+
+
+type TextData
+    = TextData String
+
+
+
+-- needs to have tools and other stuff
+
+
+type Page
+    = GraphicalPage PageData
+    | BlockPage PageData
+    | TextPage TextData
+
+
+type PageData
+    = PageData PageId (List Tool)
+
+
+type alias ExpositionContents =
+    { pages : Page
+    , id : ExpositionID
+    }
+
+
+type alias PageID =
+    Int
+
+
+type ToolStyle
+    = ToolStyle
+
+
+
+-- Dimensions x y w h
+
+
+type alias ToolProperties =
+    { dimensions : Dimensions
+    , style : ToolStyle
+    }
+
+
+type alias SimpleTextData =
+    { toolproperties : ToolProperties
+    , text : String
+    }
+
+
+type ToolId
+    = ToolId String
+
+
+type Tool
+    = SimpleText ToolId String
+    | HtmlText String
+
+
+
+-- add more
+
+
+type Exposition
+    = Exposition (List Page)
+
+
+textTools : D.Decoder (Dict PageID (List Tool))
+textTools =
+    let
+        page =
+            D.field "id" D.string |> D.map (\i -> SimpleText (ToolId i) "")
+
+        pages =
+            D.list page
+    in
+    D.keyValuePairs pages
+        |> D.andThen
+            (\lst ->
+                lst
+                    |> List.map (Tuple.mapFirst (String.toInt |> Maybe.withDefault 0))
+                    |> Dict.fromList
+            )
+
+
+page : D.Decoder Page
+page =
+    D.field "tool-text"
